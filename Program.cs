8be86088d7cfca0f40ce68500858da490b2864af
maria lúcio <dudarina.lucio@gmail.com>
2022-03-30 08:56:05 -0300

    solicita informacoes do usuario, calcula e apresenta o IMC

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("-----CALCULADORA IMC-----");
+
+            Console.WriteLine("Qual o seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("\nQual a sua altura em metros? Ex: 1,65");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("\nQual o seu peso em Kg? Ex: 57,4");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double imc = peso / (altura*altura);
+
+            Console.WriteLine("\n{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
+
+            Console.WriteLine("Seu IMC é {0}", imc);
         }
     }
 }
